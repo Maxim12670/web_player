1630dcd27c15430ae631288dfde1718f34bcd3e1
Максим <akaikinmax@mail.ru>
2024-12-18 15:19:53 +0700

    для CreateNewPlaylist добавил роутинг

--- client/src/app/router/router.tsx
+++ client/src/app/router/router.tsx
@@ -1,47 +1,59 @@
 import { createBrowserRouter, replace } from "react-router-dom";
-import { MainPage, LoginPage, HomePage, SettingPage, AddNewTrack } from "@pages/index";
+import { MainPage, LoginPage, HomePage, SettingPage, AddNewTrack, CreateNewPlaylist } from "@pages/index";
 
 export class RoutePaths {
-    static auth = "/auth";
-    static main = "/";
-    static homePage = "home";
-    static addNewTrack = "add-new-track";
-    static settingPage = "setting";
+  static auth = "/auth";
+  static main = "/";
+  static homePage = "home";
+  static addNewTrack = "add-new-track";
+  static settingPage = "setting";
+  static createNewPlaylist = "create-new-playlist";
 }
 
 const loginPage = {
-    path: RoutePaths.auth,
-    element: <LoginPage />,
+  path: RoutePaths.auth,
+  element: <LoginPage />,
 };
 
 const homePage = {
-    path: RoutePaths.homePage,
-    element: <HomePage />,
+  path: RoutePaths.homePage,
+  element: <HomePage />,
 };
 
 const addNewTrack = {
-    path: RoutePaths.addNewTrack,
-    element: <AddNewTrack />,
+  path: RoutePaths.addNewTrack,
+  element: <AddNewTrack />,
+};
+
+const createNewPlaylist = {
+  path: RoutePaths.createNewPlaylist,
+  element: <CreateNewPlaylist />,
 };
 
 const settingPage = {
-    path: RoutePaths.settingPage,
-    element: <SettingPage />,
+  path: RoutePaths.settingPage,
+  element: <SettingPage />,
 };
 
 const mainPage = {
-    path: RoutePaths.main,
-    element: <MainPage />,
-    children: [{ path: "", loader: () => replace(RoutePaths.homePage) }, homePage, addNewTrack, settingPage],
-    loader: () => {
-        if (!localStorage.getItem("userId")) {
-            console.log("я на авторизацию");
-            return replace(RoutePaths.auth);
-            // получение информации о пользователе по id
-        }
-        console.log("я на главной");
-        return null;
-    },
+  path: RoutePaths.main,
+  element: <MainPage />,
+  children: [
+    { path: "", loader: () => replace(RoutePaths.homePage) },
+    homePage,
+    addNewTrack,
+    settingPage,
+    createNewPlaylist,
+  ],
+  loader: () => {
+    if (!localStorage.getItem("userId")) {
+      console.log("я на авторизацию");
+      return replace(RoutePaths.auth);
+      // получение информации о пользователе по id
+    }
+    console.log("я на главной");
+    return null;
+  },
 };
 
 const router = createBrowserRouter([mainPage, loginPage]);
--- client/src/widgets/ui/NavMenu/NavMenu.tsx
+++ client/src/widgets/ui/NavMenu/NavMenu.tsx
@@ -4,46 +4,48 @@ import { useNavigate } from "react-router-dom";
 import { RoutePaths } from "@app/router/router";
 
 const NavMenu = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleClick = (path: string) => {
-        navigate(path);
-    };
+  const handleClick = (path: string) => {
+    navigate(path);
+  };
 
-    return (
-        <nav className={styles.menu}>
-            <div className={styles.menu__wrapper}>
-                <img src={userLogo} />
-                <span>MaximAka</span>
-            </div>
+  return (
+    <nav className={styles.menu}>
+      <div className={styles.menu__wrapper}>
+        <img src={userLogo} />
+        <span>MaximAka</span>
+      </div>
 
-            <ul className={styles.sidebar}>
-                <li className={styles.sidebar__item} onClick={() => handleClick(RoutePaths.homePage)}>
-                    Home
-                </li>
-                <li className={styles.sidebar__item} onClick={() => handleClick(RoutePaths.addNewTrack)}>
-                    Add new track
-                </li>
-                <li className={styles.sidebar__item}>Create new playlists</li>
-                <li className={styles.sidebar__item} onClick={() => handleClick(RoutePaths.settingPage)}>
-                    Settings
-                </li>
-            </ul>
+      <ul className={styles.sidebar}>
+        <li className={styles.sidebar__item} onClick={() => handleClick(RoutePaths.homePage)}>
+          Home
+        </li>
+        <li className={styles.sidebar__item} onClick={() => handleClick(RoutePaths.addNewTrack)}>
+          Add new track
+        </li>
+        <li className={styles.sidebar__item} onClick={() => handleClick(RoutePaths.createNewPlaylist)}>
+          Create new playlists
+        </li>
+        <li className={styles.sidebar__item} onClick={() => handleClick(RoutePaths.settingPage)}>
+          Settings
+        </li>
+      </ul>
 
-            <ul className={styles.sidebar}>
-                <p className={styles.sidebar__title}>My collection</p>
-                <li className={`${styles.sidebar__item} ${styles.sidebar__item_small}`}>Tracks</li>
-                <li className={`${styles.sidebar__item} ${styles.sidebar__item_small}`}>Playlists</li>
-            </ul>
+      <ul className={styles.sidebar}>
+        <p className={styles.sidebar__title}>My collection</p>
+        <li className={`${styles.sidebar__item} ${styles.sidebar__item_small}`}>Tracks</li>
+        <li className={`${styles.sidebar__item} ${styles.sidebar__item_small}`}>Playlists</li>
+      </ul>
 
-            <ul className={styles.sidebar}>
-                <p className={styles.sidebar__title}>My Playlists</p>
-                <li className={`${styles.sidebar__item} ${styles.sidebar__item_small}`}>Playlists 1</li>
-                <li className={`${styles.sidebar__item} ${styles.sidebar__item_small}`}>Playlists 2</li>
-                <li className={`${styles.sidebar__item} ${styles.sidebar__item_small}`}>Playlists 2</li>
-            </ul>
-        </nav>
-    );
+      <ul className={styles.sidebar}>
+        <p className={styles.sidebar__title}>My Playlists</p>
+        <li className={`${styles.sidebar__item} ${styles.sidebar__item_small}`}>Playlists 1</li>
+        <li className={`${styles.sidebar__item} ${styles.sidebar__item_small}`}>Playlists 2</li>
+        <li className={`${styles.sidebar__item} ${styles.sidebar__item_small}`}>Playlists 2</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavMenu;
